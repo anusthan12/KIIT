e2f98d4b3d167f5ac8285e189f0a0aa45a55c05c
Anusthan Singh <102304867+anusthan12@users.noreply.github.com>
2023-03-12 09:06:49 +0530

    Add files via upload

---
+++ TLL labwork/lab3/q2.py
@@ -0,0 +1,18 @@
+a1=int(input("Enter num1= "))
+a2=int(input("Enter num2= "))
+a3=int(input("Enter num3= "))
+a4=int(input("Enter num4= "))
+a5=int(input("Enter num5= "))
+
+min=a1
+if(a2<min):
+    min=a2
+if(a3<min):
+    min=a3
+if(a4<min):
+    min=a4
+if(a5<min):
+    min=a5
+    
+    
+print("smallest number = ", min)    
